3f702bf8e47111780f3f4c79b2434abf860da171
FarhanRiziq01 <farhanriziq01@gmail.com>
2023-11-27 19:56:35 +0700

    reg

--- function.go
+++ function.go
@@ -1,18 +1,16 @@
 package backendgcf
 
 import (
-	pasproj "github.com/HRMonitorr/PasetoprojectBackend"
 	"github.com/whatsauth/watoken"
-	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func InsertUserdata(MongoConn *mongo.Database, username, password string) (InsertedID interface{}) {
-	req := new(RegisterStruct)
-	req.Username = username
-	req.Password = password
-	return pasproj.InsertOneDoc(MongoConn, "user", req)
-}
+// func InsertUserdata(MongoConn *mongo.Database, username, password string) (InsertedID interface{}) {
+// 	req := new(RegisterStruct)
+// 	req.Username = username
+// 	req.Password = password
+// 	return pasproj.InsertOneDoc(MongoConn, "user", req)
+// }
 
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
